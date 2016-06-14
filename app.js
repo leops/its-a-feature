e362ab74a77a991f777edbb6b8506fb9d9ba24be
Sboob <antho10440@hotmail.fr>
2016-06-14 15:19:59 +0200

    Body parsrr

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
+const User = require('./models/user');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cookieParser('secret'));
 app.use(cookieSession({
     keys: ['azerty', 'qwerty']
@@ -14,6 +18,18 @@ app.use(cookieSession({
 
 const apiRouter = require('./api');
 app.use('/api', apiRouter);
+app.post('/login', (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    User.findOne({
+        username
+    }).then(user => {
+        console.log(user);
+    }).catch(err => {
+        res.status(500).end();
+    })
+    res.send('Hello World!');
+});
 
 /*const {
     hash,
