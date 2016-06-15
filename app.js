011ce8a8e068170676b9a61271b35b571629d60c
Sboob <antho10440@hotmail.fr>
2016-06-15 18:32:50 +0200

    rm log

--- app.js
+++ app.js
@@ -11,10 +11,6 @@ app.use(bodyParser.json());
 const apiRouter = require('./api');
 app.use('/api', apiRouter(io));
 
-io.on('connection', socket => {
-    console.log('New connection');
-});
-
 server.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
