51d8b4b03c0fed2f5808fa5830664dcbab3ce76b
Léo-Paul COUTURIER <leopaul3@gmail.com>
2016-06-15 12:04:00 +0200

    Remove logout route and urlencoded bodyparser

--- app.js
+++ app.js
@@ -5,17 +5,9 @@ const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 const apiRouter = require('./api');
 app.use('/api', apiRouter);
-
-app.get('/logout', (req, res) => {
-    req.session = null;
-    res.redirect('/#/login');
-});
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
